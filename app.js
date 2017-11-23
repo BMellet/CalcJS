524b311faa12f181d20d7bab00ee1538d76d07a2
BMellet <benzero.m@gmail.com>
2017-11-23 14:27:36 +0100

    ajout de tout les chiffres

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ function suppr()
 	document.getElementById("resultat").value= valeur;
 }
 
+function zero()
+{
+
+	var x= document.getElementById("zero").value;
+	console.log(x);
+	var valeur=document.getElementById("resultat").value;
+	document.getElementById("resultat").value=valeur+x;
+}
+
 function un()
 {
 
@@ -16,3 +25,86 @@ function un()
 	var valeur=document.getElementById("resultat").value;
 	document.getElementById("resultat").value=valeur+x;
 }
+function deux()
+{
+
+	var x= document.getElementById("deux").value;
+	console.log(x);
+	var valeur=document.getElementById("resultat").value;
+	document.getElementById("resultat").value=valeur+x;
+}
+
+function trois()
+{
+
+	var x= document.getElementById("trois").value;
+	console.log(x);
+	var valeur=document.getElementById("resultat").value;
+	document.getElementById("resultat").value=valeur+x;
+}
+
+function quatre()
+{
+
+	var x= document.getElementById("quatre").value;
+	console.log(x);
+	var valeur=document.getElementById("resultat").value;
+	document.getElementById("resultat").value=valeur+x;
+}
+
+function cinq()
+{
+
+	var x= document.getElementById("cinq").value;
+	console.log(x);
+	var valeur=document.getElementById("resultat").value;
+	document.getElementById("resultat").value=valeur+x;
+}
+
+function six()
+{
+
+	var x= document.getElementById("six").value;
+	console.log(x);
+	var valeur=document.getElementById("resultat").value;
+	document.getElementById("resultat").value=valeur+x;
+}
+
+function sept()
+{
+
+	var x= document.getElementById("sept").value;
+	console.log(x);
+	var valeur=document.getElementById("resultat").value;
+	document.getElementById("resultat").value=valeur+x;
+}
+
+function huit()
+{
+
+	var x= document.getElementById("huit").value;
+	console.log(x);
+	var valeur=document.getElementById("resultat").value;
+	document.getElementById("resultat").value=valeur+x;
+}
+
+function neuf()
+{
+
+	var x= document.getElementById("neuf").value;
+	console.log(x);
+	var valeur=document.getElementById("resultat").value;
+	document.getElementById("resultat").value=valeur+x;
+}
+
+
+
+//FONCTION DE CALCUL 
+
+function egal()
+{
+	var x= document.getElementById("resultat").value;
+	
+}
+
+
